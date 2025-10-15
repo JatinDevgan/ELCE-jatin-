dfdeba1770e5022215e2a5f912f36f5efe934d1b
Jatin Devgan <jatindevgan071@gmail.com>
2025-10-15 16:48:46 +0530

    my first file

---
+++ dsa/castin.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    // int grade = 'b'; 
+    // int value = grade ;
+    cout<<int('b')  ;
+return 0 ;
+  }
---
+++ dsa/op.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    int a = 10 , b = 3 ;
+    // ARITHMETIC OPERATORS 
+    cout<<"arithematic operators "<< endl;
+    cout<< "a + b"<< (a+b)<<endl ; 
+cout << "a-b"<< (a-b)<< endl ;
+cout << "a*b"<<(a*b)<<endl;
+cout << "a/b"<<(a/b)<<endl;
+cout<< "a%b"<< (a%b)<<endl;
+cout << "++a"<<(++a)<<endl;
+cout << "b++"<<(b++)<<endl;
+cout<< "-a"<<(-a)<<endl;
+cout<<"b--"<<(b--)<<endl;
+cout<< "realational operators " <<endl;
+cout<<"a==b"<<(a==b)<<endl;
+cout<<(a!=b)<<(a!=b)<<endl;
+cout<<(a>b)<<(a>b)<<endl;
+cout<<(a<b)<<(a<b)<<endl;
+cout<<(a>=b)<<(a>=b)<<endl;
+cout<<(a<=b)<<(a<=b)<<endl;
+cout<<"logical operatord" <<endl;
+bool  x = true , y = false ;
+cout <<(x&&y)<<(x&&y)<<endl;
+ //cout<<(ab)<<(ax)<<endl;
+ cout<<(!x)<<(!x)<<endl;
+ cout<<"ASSINGMENT OPERATORS" <<endl ;
+ int c =5;
+ cout << "c="<<c << endl ;  
+
+}
